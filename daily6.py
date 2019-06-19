f80fdc20fc9f1b30d0b53f6716d910820ed167c1
Abhijit Roy <jitroy160@gmail.com>
2019-06-19 16:50:03 +0530

    b19

---
+++ daily6.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(Abhijit)s
+"""
+
+list={"Name","Roll Number","Section","Department","Address"}
+list
+print(list)
+
+math.pi
+import math
+print(math.pi)
+
+import random
+
+for i in range(10): print(random.randint(1,25), end ='')
