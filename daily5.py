2d4930b8985659915a6d557764f00b58714336f4
Abhijit Roy <jitroy160@gmail.com>
2019-06-19 15:14:37 +0530

    a19

---
+++ daily5.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on %(date)s
+
+@author: %(Abhijit)s
+"""
+
+set= {"India","England","Australia","NewZeland","South Africa","Bangladesh","WestIndies","Srilanka","Afganisthan","England"}
+set
+print(set)
+
+set= {"India","England","Australia","NewZeland","South Africa","Bangladesh","WestIndies","Srilanka","Afganisthan","England"}
+set.remove("England")
+print(set)
+
+set= {"India","Australia","NewZeland","South Africa","Bangladesh","WestIndies","Srilanka","Afganisthan"}
+set.add("England")
+print(set)
+
+set= {"India","England","Australia","NewZeland","South Africa","Bangladesh","WestIndies","Srilanka","Afganisthan"}
+set.add("Pakistan")
+print(set)
